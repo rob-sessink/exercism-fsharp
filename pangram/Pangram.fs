6a68b0206d6760797b994ad8e8fc86b43cc3872d
Rob Sessink <rob.sessink@yucca.io>
2020-10-24 20:03:52 +0200

    EXEC: added pangram assignment

---
+++ pangram/Pangram.fs
@@ -0,0 +1,20 @@
+﻿module Pangram
+
+let alphabet = seq [ 'a' .. 'z' ]
+
+let inSeq (c: char) (input: seq<char>) = input |> Seq.contains c
+
+let onlyLowercaseAlphabet (input: string) =
+    input.ToLower()
+    |> Seq.filter (fun c -> inSeq c alphabet)
+
+let isPangram (input: string): bool =
+    alphabet
+    |> Seq.forall (fun c -> inSeq c (onlyLowercaseAlphabet input))
+
+// alternative implementation using uniq | sort | forall2
+let isPangram2 (input: string): bool =
+    onlyLowercaseAlphabet input
+    |> Seq.distinct
+    |> Seq.sort
+    |> Seq.forall2 (fun l1 l2 -> l1 = l2) alphabet
