f0972ad3f17ba712d7068bcf15dbe666dbf2d4bf
Rob Sessink <rob.sessink@yucca.io>
2020-10-24 20:44:06 +0200

    EXEC: added isogram assignment

---
+++ isogram/Isogram.fs
@@ -0,0 +1,14 @@
+﻿module Isogram
+
+open System
+
+
+let isIsogram (input: string) =
+    match input with
+    | "" -> true
+    | i ->
+        i.ToLower()
+        |> Seq.filter Char.IsLetter
+        |> Seq.countBy (fun c -> c)
+        |> Seq.maxBy snd
+        |> snd = 1
