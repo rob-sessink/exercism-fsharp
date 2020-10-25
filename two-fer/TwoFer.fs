195bd3fe2069a13a417b91539b81f77d90d14c4a
Rob Sessink <rob.sessink@yucca.io>
2020-10-25 19:27:22 +0100

    EXEC: refactor to use Option.defaultValue

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -2,7 +2,6 @@
 
 let twoFer (input: string option): string =
     let twofer =
-        match input with
-        | Some name -> sprintf "One for %s, one for me." name
-        | None _ -> "One for you, one for me."
+        sprintf "One for %s, one for me." (Option.defaultValue "you" input)
+
     twofer
