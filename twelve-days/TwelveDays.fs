e3da2565c76c7e28df9eef4c42bee9c9260d2e1c
Rob Sessink <rob.sessink@yucca.io>
2020-10-26 21:31:45 +0100

    EXEC: add match when clauses add add formatting so this can be removed out of the gift list

--- twelve-days/TwelveDays.fs
+++ twelve-days/TwelveDays.fs
@@ -16,28 +16,30 @@ let recite start stop =
           "twelfth" ]
 
     let gifts =
-        [ " a Partridge in a Pear Tree."
-          " two Turtle Doves, and"
-          " three French Hens,"
-          " four Calling Birds,"
-          " five Gold Rings,"
-          " six Geese-a-Laying,"
-          " seven Swans-a-Swimming,"
-          " eight Maids-a-Milking,"
-          " nine Ladies Dancing,"
-          " ten Lords-a-Leaping,"
-          " eleven Pipers Piping,"
-          " twelve Drummers Drumming," ]
+        [ "a Partridge in a Pear Tree"
+          "two Turtle Doves"
+          "three French Hens"
+          "four Calling Birds"
+          "five Gold Rings"
+          "six Geese-a-Laying"
+          "seven Swans-a-Swimming"
+          "eight Maids-a-Milking"
+          "nine Ladies Dancing"
+          "ten Lords-a-Leaping"
+          "eleven Pipers Piping"
+          "twelve Drummers Drumming" ]
 
-    let rec appendGifts gifts giftList =
+    let rec appendGifts gifts present giftList =
         match gifts with
-        | head :: tail -> appendGifts tail (sprintf "%s%s" head giftList)
+        | head :: tail when present = 0 -> appendGifts tail (present + 1) (sprintf "%s%s." head giftList)
+        | head :: tail when present = 1 -> appendGifts tail (present + 1) (sprintf "%s, and %s" head giftList)
+        | head :: tail -> appendGifts tail (present + 1) (sprintf "%s, %s" head giftList)
         | [] -> giftList
 
     let prefix day =
-        sprintf "On the %s day of Christmas my true love gave to me:" days.[day]
+        sprintf "On the %s day of Christmas my true love gave to me: " days.[day]
 
-    let listOfGifts day = appendGifts gifts.[0..day] ""
+    let listOfGifts day = appendGifts gifts.[0..day] 0 ""
 
     let verse day =
         (sprintf "%s%s" (prefix day) (listOfGifts day))
