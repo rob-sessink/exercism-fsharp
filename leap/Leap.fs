37a5f0c170765ddc4b834e257e4f52284b12fbf5
Rob Sessink <rob.sessink@yucca.io>
2020-10-27 21:59:19 +0100

    EXEC: add assignment leap

---
+++ leap/Leap.fs
@@ -0,0 +1,12 @@
+﻿module Leap
+
+let leapYear (year: int): bool =
+    match year % 4 with
+    | 0 ->
+        match year % 100 with
+        | 0 ->
+            match year % 400 with
+            | 0 -> true
+            | _ -> false
+        | _ -> true
+    | _ -> false
