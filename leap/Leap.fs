4f71b2168ed866b0a1bde2e3a108e6a579385cfb
Rob Sessink <rob.sessink@yucca.io>
2020-10-28 21:06:38 +0100

    EXEC: simplify statements

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,12 +1,17 @@
 ﻿module Leap
 
+//  4     |  100  | 400          | leap year 
+//  true  | true  | true         | true      
+//  true  | true  | false        | false      
+//  true  | false | false        | true       
+//  false | false | false        | false
+
 let leapYear (year: int): bool =
-    match year % 4 with
-    | 0 ->
-        match year % 100 with
-        | 0 ->
-            match year % 400 with
-            | 0 -> true
-            | _ -> false
-        | _ -> true
-    | _ -> false
+
+    let modBy divisor year = year % divisor
+    let modBy4 year = modBy 4 year
+    let modBy100 year = modBy 100 year
+    let modBy400 year = modBy 400 year
+
+    modBy4 year = 0
+    && not (modBy100 year = 0) || modBy400 year = 0
