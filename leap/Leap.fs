61286089f2eb214705db402726512020fb4098ac
Rob Sessink <rob.sessink@yucca.io>
2020-10-28 21:08:15 +0100

    EXEC: move remained test into function

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,17 +1,18 @@
 ﻿module Leap
 
-//  4     |  100  | 400          | leap year 
-//  true  | true  | true         | true      
-//  true  | true  | false        | false      
-//  true  | false | false        | true       
+//  4     |  100  | 400          | leap year
+//  true  | true  | true         | true
+//  true  | true  | false        | false
+//  true  | false | false        | true
 //  false | false | false        | false
 
 let leapYear (year: int): bool =
 
-    let modBy divisor year = year % divisor
+    let modBy divisor year = year % divisor = 0
     let modBy4 year = modBy 4 year
     let modBy100 year = modBy 100 year
     let modBy400 year = modBy 400 year
 
-    modBy4 year = 0
-    && not (modBy100 year = 0) || modBy400 year = 0
+    modBy4 year
+    && not (modBy100 year)
+    || modBy400 year
