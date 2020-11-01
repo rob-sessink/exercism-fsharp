12a087100a22bee8da100a7e8fa0c667c83ffd51
Rob Sessink <rob.sessink@yucca.io>
2020-11-01 12:45:48 +0100

    EXEC: add assignment grains

---
+++ grains/Grains.fs
@@ -0,0 +1,20 @@
+﻿module Grains
+
+let rec squared acc i =
+    match i with
+    | 0
+    | 1 -> acc
+    | _ -> squared (acc * bigint (2)) (i - 1)
+
+let toUInt64 n = Ok(uint64 n)
+
+let square (n: int): Result<uint64, string> =
+    match n with
+    | n when (n < 0 || n > 64) -> Error "square must be between 1 and 64"
+    | 0 -> Error "square must be between 1 and 64"
+    | n -> toUInt64 (squared bigint.One n)
+
+let total: Result<uint64, string> =
+    seq { 1 .. 64 }
+    |> Seq.sumBy (squared bigint.One)
+    |> toUInt64
