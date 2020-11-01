007b04f72f20c5a9290ef1ab9ae5ac901cf4329b
Rob Sessink <rob.sessink@yucca.io>
2020-11-01 12:55:09 +0100

    EXEC: improve output casting

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -6,14 +6,13 @@ let rec squared acc i =
     | 1 -> acc
     | _ -> squared (acc * bigint (2)) (i - 1)
 
-let toUInt64 n = Ok(uint64 n)
-
 let square (n: int): Result<uint64, string> =
     match n with
     | n when (n <= 0 || n > 64) -> Error "square must be between 1 and 64"
-    | n -> toUInt64 (squared bigint.One n)
+    | n -> (squared bigint.One n) |> uint64 |> Ok
 
 let total: Result<uint64, string> =
     seq { 1 .. 64 }
     |> Seq.sumBy (squared bigint.One)
-    |> toUInt64
+    |> uint64
+    |> Ok
