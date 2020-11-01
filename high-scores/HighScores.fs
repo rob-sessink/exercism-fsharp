c035c5242eea728979d11e70190194836a726d6c
Rob Sessink <rob.sessink@yucca.io>
2020-11-01 21:02:26 +0100

    EXEC: add assignment HighScores

---
+++ high-scores/HighScores.fs
@@ -0,0 +1,12 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+
+let latest (values: int list): int = values |> List.last
+
+let personalBest (values: int list): int = values |> List.max
+
+let personalTopThree (values: int list): int list =
+    values
+    |> List.sortDescending
+    |> List.take (min values.Length 3)
