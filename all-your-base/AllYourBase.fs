b3d9937b839cd779f5705171aac96ac0929f85fe
Rob Sessink <rob.sessink@yucca.io>
2020-11-04 21:43:49 +0100

    EXEC: improve function naming and align parameter to allow forward piping

--- all-your-base/AllYourBase.fs
+++ all-your-base/AllYourBase.fs
@@ -6,20 +6,20 @@ let rebase digits inputBase outputBase =
 
     // convert digit to positional notation based upon inputBase
     // [123] = 1 * 10^2 + 2 * 10^1 + 3 * 10^0
-    let toPositional digits inputBase =
+    let fromBase digits inputBase =
         digits
         |> List.rev
         |> List.indexed
         |> List.fold (fun acc elem -> acc + (snd elem) * pown inputBase (fst elem)) 0
 
-    // divide by number base collecting remainders
-    let rec baseConversion number outputBase acc =
+    // divide number by base; collecting remainders
+    let rec toBase outputBase acc number =
         let division = number / outputBase
         let remainder = number % outputBase
 
         match (division, remainder) with
         | 0, x -> (x :: acc)
-        | _, x -> baseConversion division outputBase (x :: acc)
+        | _, x -> baseConversion outputBase (x :: acc) division
 
     // validate input parameters
     match digits, inputBase, outputBase with
@@ -27,6 +27,4 @@ let rebase digits inputBase outputBase =
     | (d, i, _) when d |> List.exists (fun x -> x >= i) -> None // digit equal or larger than base
     | (_, i, _) when i <= 1 -> None // input base smaller or equal to 1
     | (_, _, o) when o <= 1 -> None // output base smaller or equal to 1
-    | (d, i, o) ->
-        let number = toPositional d i
-        (baseConversion number o []) |> Some
+    | (d, i, o) -> fromBase d i |> toBase o [] |> Some
