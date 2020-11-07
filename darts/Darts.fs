559910f122c1acde4228bfcc860123c711f87b40
Rob Sessink <rob.sessink@yucca.io>
2020-11-07 15:44:20 +0100

    EXER: add darts assignment

---
+++ darts/Darts.fs
@@ -0,0 +1,16 @@
+﻿module Darts
+
+// inclusive cartesian on upper bound and when lower bound = 0
+let (>=<) x (min, max) =
+    match min with
+    | 0.0 -> x >= min * min && x <= max * max
+    | _ -> x > min * min && x <= max * max
+
+let score (x: double) (y: double): int =
+    let real = x * x + y * y
+
+    match real with
+    | r when r >=< (5.0, 10.0) -> 1
+    | r when r >=< (1.0, 5.0) -> 5
+    | r when r >=< (0.0, 1.0) -> 10
+    | _ -> 0
