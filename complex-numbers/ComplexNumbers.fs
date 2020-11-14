0b5700a7f973eb3ab497adb3961b4234a7a10b58
Rob Sessink <rob.sessink@yucca.io>
2020-11-14 21:31:36 +0100

    EXER: uppercase record labels

--- complex-numbers/ComplexNumbers.fs
+++ complex-numbers/ComplexNumbers.fs
@@ -4,44 +4,44 @@
 
 open System
 
-type ComplexNumber = { real: float; imaginary: float }
+type ComplexNumber = { Real: float; Imaginary: float }
 
 
-let create real imaginary = { real = real; imaginary = imaginary }
+let create real imaginary = { Real = real; Imaginary = imaginary }
 
 let mul z1 z2 =
-    { real =
-          (z1.real * z2.real)
-          + (z1.imaginary * z2.imaginary * -1.)
-      imaginary =
-          (z1.real * z2.imaginary)
-          + (z1.imaginary * z2.real) }
+    { Real =
+          (z1.Real * z2.Real)
+          + (z1.Imaginary * z2.Imaginary * -1.)
+      Imaginary =
+          (z1.Real * z2.Imaginary)
+          + (z1.Imaginary * z2.Real) }
 
 let add z1 z2 =
-    { real = z1.real + z2.real
-      imaginary = z1.imaginary + z2.imaginary }
+    { Real = z1.Real + z2.Real
+      Imaginary = z1.Imaginary + z2.Imaginary }
 
 let sub z1 z2 =
-    { real = z1.real - z2.real
-      imaginary = z1.imaginary - z2.imaginary }
+    { Real = z1.Real - z2.Real
+      Imaginary = z1.Imaginary - z2.Imaginary }
 
 let abs z =
-    sqrt (pown z.real 2 + pown z.imaginary 2)
+    sqrt (pown z.Real 2 + pown z.Imaginary 2)
 
 let conjugate z =
-    { real = z.real
-      imaginary = z.imaginary * -1. }
+    { Real = z.Real
+      Imaginary = z.Imaginary * -1. }
 
 let div z1 z2 =
     let top = mul z1 (conjugate z2)
     let bottom = mul z2 (conjugate z2)
-    { real = top.real / bottom.real
-      imaginary = top.imaginary / bottom.real }
+    { Real = top.Real / bottom.Real
+      Imaginary = top.Imaginary / bottom.Real }
 
-let real z = z.real
+let real z = z.Real
 
-let imaginary z = z.imaginary
+let imaginary z = z.Imaginary
 
 let exp z =
-    { real = Math.Exp(z.real) * Math.Cos(z.imaginary)
-      imaginary = Math.Exp(z.real) * Math.Sin(z.imaginary) }
+    { Real = Math.Exp(z.Real) * Math.Cos(z.Imaginary)
+      Imaginary = Math.Exp(z.Real) * Math.Sin(z.Imaginary) }
