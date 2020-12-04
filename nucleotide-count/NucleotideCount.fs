3ace7579a46e782d0063ccb25ca301d73101811c
Rob Sessink <rob.sessink@yucca.io>
2020-12-04 21:31:17 +0100

    EXER: add assignment nucleotide-count

---
+++ nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,20 @@
+﻿module NucleotideCount
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+
+    let nucleotides = seq [ 'A'; 'C'; 'G'; 'T' ]
+
+    let zeroed =
+        Seq.fold (fun (map: Map<char, int>) t -> map.Add(t, 0)) Map.empty nucleotides
+
+    let valid strand =
+        Seq.forall (fun nu -> (Seq.contains nu nucleotides)) strand
+
+    let count strand =
+        strand
+        |> Seq.countBy (fun nu -> nu)
+        |> Seq.fold (fun (map: Map<char, int>) t -> map.Add(fst t, snd t)) zeroed
+
+    match valid strand with
+    | true -> Some(count strand)
+    | false -> None
