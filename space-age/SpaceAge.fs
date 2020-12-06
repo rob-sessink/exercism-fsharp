435153dbcce3e2733d95f1021011b99c5dce0ed9
Rob Sessink <rob.sessink@yucca.io>
2020-12-06 11:37:55 +0100

    EXER: proper OO style ;-)

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,6 +1,13 @@
 ﻿module SpaceAge
 
-type Planet = { orbitalPeriod: float }
+type Planet =
+    { orbitalPeriod: float }
+    member this.secondsPerEarthYear = 31557600.0
+
+    member this.ageInEarthYears(seconds: int64) =
+        float seconds
+        / this.secondsPerEarthYear
+        / this.orbitalPeriod
 
 let Mercury = { orbitalPeriod = 0.2408467 }
 let Venus = { orbitalPeriod = 0.61519726 }
@@ -10,9 +17,5 @@ let Jupiter = { orbitalPeriod = 11.862615 }
 let Saturn = { orbitalPeriod = 29.447498 }
 let Uranus = { orbitalPeriod = 84.016846 }
 let Neptune = { orbitalPeriod = 164.79132 }
-let secondsPerEarthYear = 31557600.0
 
-let age (planet: Planet) (seconds: int64): float =
-    float seconds
-    / secondsPerEarthYear
-    / planet.orbitalPeriod
+let age (planet: Planet) (seconds: int64): float = planet.ageInEarthYears seconds
