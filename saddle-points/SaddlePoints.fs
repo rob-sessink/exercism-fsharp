ad02f23fbbf6eec4d31e267d0cfb4b599cef0b54
Rob Sessink <rob.sessink@yucca.io>
2020-12-13 11:15:00 +0100

    EXER: condense and format

--- saddle-points/SaddlePoints.fs
+++ saddle-points/SaddlePoints.fs
@@ -10,36 +10,24 @@ let elements sorterFunc array =
     |> List.map snd
     |> List.concat
 
-// get greatest entries in an array returned as indexed values list
 let greatest array = elements List.sortDescending array
 
-// get smallest entries in an array returned as indexed values list
 let smallest array = elements List.sort array
 
-let column (a2d: int [,]) coli = a2d.[*, coli]
+let smallestForColumn (matrix: int [,]) colIndex = matrix.[*, colIndex] |> smallest
 
-// find smallest entries for a column in matrix
-let smallestForColumn matrix colIndex =
-    let column = column matrix colIndex
-    smallest column
+let filterByIndex row entries = entries |> List.filter (fun c -> fst c = row)
 
-// filter a list of entry by index (row or column)
-let filterByIndex row entries =
-    entries |> List.filter (fun c -> fst c = row)
-
-// determine if row/column combination is a saddle by comparison of row and column values, if equal return SaddlePoint
 let isSaddle rowPosition colPosition =
     if (snd rowPosition) = (snd colPosition)
     then Some(fst colPosition + 1, fst rowPosition + 1)
     else None
 
-let chooseSaddles row columns =
-    List.choose (fun column -> isSaddle row column) columns
+let chooseSaddles row columns = List.choose (fun column -> isSaddle row column) columns
 
 let findSaddlePoints matrix =
-    let rows = (Array2D.length1 matrix) - 1
     seq {
-        for row in 0 .. rows do
+        for row in 0 .. (Array2D.length1 matrix) - 1 do
             yield! matrix.[row, *]
                    |> greatest
                    |> List.map (fun rowPosition ->
