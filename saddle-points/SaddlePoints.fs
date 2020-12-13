f79918994585c603bdcc519bf8751525d12f7bbe
Rob Sessink <rob.sessink@yucca.io>
2020-12-13 21:20:23 +0100

    EXER: simplify greatest and smallest functions and unnecessary match

--- saddle-points/SaddlePoints.fs
+++ saddle-points/SaddlePoints.fs
@@ -1,18 +1,16 @@
 module SaddlePoints
 
-let elements sorterFunc array =
+let greatest array =
     array
     |> List.ofArray
     |> List.indexed
-    |> List.groupBy snd
-    |> sorterFunc
-    |> List.take 1
-    |> List.map snd
-    |> List.concat
+    |> List.choose (fun e -> if (snd e) = (Array.max array) then Some e else None)
 
-let greatest array = elements List.sortDescending array
-
-let smallest array = elements List.sort array
+let smallest array =
+    array
+    |> List.ofArray
+    |> List.indexed
+    |> List.choose (fun e -> if (snd e) = (Array.min array) then Some e else None)
 
 let smallestInColumn (matrix: int [,]) colIndex = matrix.[*, colIndex] |> smallest
 
@@ -22,20 +20,16 @@ let filterByIndex index entries =
 let createSaddle rowPosition colPosition =
     (fst colPosition + 1, fst rowPosition + 1)
 
-let findSaddlePoints matrix =
+let saddlePoints (matrix: int list list) =
+    let a2d = array2D matrix
     seq {
-        for row in 0 .. (Array2D.length1 matrix) - 1 do
-            yield! matrix.[row, *]
+        for row in 0 .. (Array2D.length1 a2d) - 1 do
+            yield! a2d.[row, *]
                    |> greatest
                    |> List.map (fun rowPosition ->
-                       smallestInColumn matrix (fst rowPosition)
+                       smallestInColumn a2d (fst rowPosition)
                        |> filterByIndex row
                        |> List.map (createSaddle rowPosition))
                    |> List.concat
     }
     |> Seq.toList
-
-let saddlePoints (matrix: int list list) =
-    match matrix with
-    | [ [] ] -> []
-    | _ -> findSaddlePoints (array2D matrix)
