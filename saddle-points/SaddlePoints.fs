e3f4fbf37225b878926cde30a5fa23d7c3c173f8
Rob Sessink <rob.sessink@yucca.io>
2020-12-14 20:50:10 +0100

    EXER: inline functions and improve variables

--- saddle-points/SaddlePoints.fs
+++ saddle-points/SaddlePoints.fs
@@ -1,30 +1,25 @@
 module SaddlePoints
 
-let getElements minOrMax array =
+let indexedFilter fExtremum array =
     array
     |> List.ofArray
     |> List.indexed
-    |> List.filter (fun e -> (snd e) = (minOrMax array))
+    |> List.filter (fun e -> snd e = fExtremum array)
 
-let smallestInColumn (matrix: int [,]) colIndex =
-    matrix.[*, colIndex] |> getElements Array.min
+let lowestInColumn (matrix: int [,]) colIdx = matrix.[*, colIdx] |> indexedFilter Array.min
 
-let filterByIndex index entries =
-    entries |> List.filter (fun c -> fst c = index)
-
-let createSaddle rowPosition colPosition =
-    (fst colPosition + 1, fst rowPosition + 1)
+let createSaddle rowPos colPos = (fst colPos + 1, fst rowPos + 1)
 
 let saddlePoints (matrix: int list list) =
     let a2d = array2D matrix
     seq {
-        for row in 0 .. (Array2D.length1 a2d) - 1 do
-            yield! a2d.[row, *]
-                   |> getElements Array.max
-                   |> List.map (fun rowPosition ->
-                       smallestInColumn a2d (fst rowPosition)
-                       |> filterByIndex row
-                       |> List.map (createSaddle rowPosition))
+        for rowIdx in 0 .. (Array2D.length1 a2d) - 1 do
+            yield! a2d.[rowIdx, *]
+                   |> indexedFilter Array.max
+                   |> List.map (fun rowPos ->
+                       lowestInColumn a2d (fst rowPos)
+                       |> List.filter (fun c -> fst c = rowIdx)
+                       |> List.map (createSaddle rowPos))
                    |> List.concat
     }
     |> Seq.toList
