5113ff6fb875aa287150a0a8f6203c28b70a41ad
Rob Sessink <rob.sessink@yucca.io>
2020-12-17 21:17:45 +0100

    EXER: use a Set to hold the options

--- grep/Grep.fs
+++ grep/Grep.fs
@@ -7,51 +7,47 @@ type File = File of file: string * lines: string list
 
 type MatchedLine = MatchedLine of lineNumber: int * line: string * fileName: string
 
-let hasFlag flag flagArguments = Seq.contains flag flagArguments
+let caseInsensitive (flags: Set<string>) pattern =
+    if flags.Contains("-i") then "(?i)" + pattern else pattern
 
-let caseInsensitive flags pattern =
-    if hasFlag "-i" flags then "(?i)" + pattern else pattern
+let entireLine (flags: Set<string>) pattern =
+    if flags.Contains("-x") then "^" + pattern + "$" else pattern
 
-let entireLine flags pattern =
-    if hasFlag "-x" flags then "^" + pattern + "$" else pattern
+let matches (flags: Set<string>) pattern lineIdx =
+    let regex = caseInsensitive flags pattern |> entireLine flags
 
-let matches flags pattern lineIdx =
-    let regex =
-        caseInsensitive flags pattern |> entireLine flags
+    let inverted = flags.Contains("-v")
+    Regex.Match((snd lineIdx), regex).Success <> inverted
 
-    let inverted = hasFlag "-v" flags
-    Regex.Match((snd lineIdx), regex).Success
-    <> inverted
+let formatMultiple (flags: Set<string>) files = List.length files > 1 && not (flags.Contains("-l"))
 
-let formatMultiple flags files =
-    List.length files > 1 && not (hasFlag "-l" flags)
-
-let format flags files matchedLine =
-    let (MatchedLine (lineNumber, line, fileName)) = matchedLine
+let format (flags: Set<string>) files matchedLine =
+    let (MatchedLine(lineNumber, line, fileName)) = matchedLine
 
     let formatted =
         match flags with
-        | f when hasFlag "-l" f -> sprintf "%s" fileName
-        | f when hasFlag "-n" f -> sprintf "%i:%s" (lineNumber + 1) line
+        | f when flags.Contains("-l") -> sprintf "%s" fileName
+        | f when flags.Contains("-n") -> sprintf "%i:%s" (lineNumber + 1) line
         | _ -> sprintf "%s" line
 
     match formatMultiple flags files with
     | false -> formatted
     | true -> sprintf "%s:%s" fileName formatted
 
-let openFile file =
-    (file, File.ReadAllLines(file) |> List.ofArray)
+let openFile file = (file, File.ReadAllLines(file) |> List.ofArray)
 
-let grep' flags pattern file =
+let grep' (flags: Set<string>) pattern file =
     snd file
     |> List.indexed
     |> List.filter (matches flags pattern)
     |> List.map (fun lineIdx -> MatchedLine(fst lineIdx, snd lineIdx, fst file))
 
 let grep files flagArguments pattern =
+    let flags = flagArguments |> set
+
     files
     |> List.map openFile
-    |> List.map (grep' flagArguments pattern)
+    |> List.map (grep' flags pattern)
     |> List.concat
-    |> List.map (format flagArguments files)
+    |> List.map (format flags files)
     |> List.distinct
