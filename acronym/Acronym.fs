9326a1766a6f30e0ebfaafcc4847448fc3c2a803
Rob Sessink <rob.sessink@yucca.io>
2021-01-09 20:41:01 +0100

    EXER: add assignment acronym

---
+++ acronym/Acronym.fs
@@ -0,0 +1,11 @@
+﻿module Acronym
+
+open System
+
+let abbreviate (phrase: string) =
+    phrase.Split([| ' '; '-'; '_' |])
+    |> Seq.filter (fun w -> w.Length > 0)
+    |> Seq.map (Seq.take 1)
+    |> Seq.concat
+    |> Seq.map Char.ToUpper
+    |> String.Concat
