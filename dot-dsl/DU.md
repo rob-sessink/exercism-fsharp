962d44772e98be275e00a94a1c70dc9437ea1143
Rob Sessink <rob.sessink@yucca.io>
2021-05-22 21:38:25 +0200

    EXER: add assignment dot-dsl

---
+++ dot-dsl/DU.md
@@ -0,0 +1,5 @@
+### Recursive Discriminated Unions with fieldnames
+https://stackoverflow.com/questions/44666981/recursive-discriminated-union-case-types/44667554
+
+### Filtering by Discriminated Union type
+https://stackoverflow.com/questions/13070487/f-use-generic-type-as-pattern-discriminator
---
+++ dot-dsl/DotDsl.fs
@@ -0,0 +1,44 @@
+﻿module DotDsl
+
+type Attribute = string * string
+type Node = string * Attribute list
+type Edge = string * string * Attribute list
+
+type Entry =
+    | Attribute of Attribute
+    | Node of Node
+    | Edge of Edge
+
+type Graph = Entry list
+
+let graph children = children
+
+let attr key value : Entry = Attribute(key, value)
+
+let node key attrs = Node(key, attrs)
+
+let edge left right attrs = Edge(left, right, attrs)
+
+let attrs graph =
+    graph
+    |> List.filter
+        (function
+        | Attribute _ -> true
+        | _ -> false)
+    |> List.sort
+
+let nodes graph =
+    graph
+    |> List.filter
+        (function
+        | Node _ -> true
+        | _ -> false)
+    |> List.sort
+
+let edges graph =
+    graph
+    |> List.filter
+        (function
+        | Edge _ -> true
+        | _ -> false)
+    |> List.sort
